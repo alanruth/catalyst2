4237a910adeb93e53f86a809305b3721d8a0538b
Alan Ruth <alanruth@live.com>
2013-08-24 01:37:24 -0600

    salesforce integration bug fix on contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,37 +5,17 @@ class Contact < ActiveRecord::Base
 	validates :email,  presence: true, format: { with: VALID_EMAIL_REGEX }
 	validates :message,  presence: true, length: { maximum: 1000 }
 
-	require "addressable/uri"
 	SALESFORCE_PATH = "https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8"
+
 	after_create :send_to_salesforce
 
 	def send_to_salesforce
-			begin
-				response = Curl::Easy.http_post(SALESFORCE_PATH, attr_for_sf)
-				self.sent_to_sales_force = response.response_code == 200
-				save
-			rescue
-				# Send notification of error in contacting Salesforce
-			end
+		begin
+			response = RestClient.post SALESFORCE_PATH, :first_name => first_name, :last_name => last_name, :oid => "00Di0000000dKbA", :retURL => "http://www.procatalystgroup.com/thank_you", :email => email, :message => message
+			self.sent_to_sales_force = response.code == 200
+		rescue => e
+			response.code
 		end
-
-
-	protected
-	def attr_for_sf
-		to_query_string({
-				                "first_name" => first_name,
-				                "last_name" => last_name,
-				                "email" => email,
-				                "description" => message,
-				                "oid" => "00Di0000000dKbA",
-				                "retURL" => "http://www.procatalystgroup.com/thankyou"
-		                })
-	end
-
-	def to_query_string(val)
-		uri = Addressable::URI.new
-		uri.query_values = val
-		uri.query
 	end
 
 end
